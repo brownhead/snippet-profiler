5b53d063d7102e04b916fde4d8837d0261564c76
brownhead <johnsullivan.pem@gmail.com>
2014-11-28 21:12:07 -0800

    First API function! Weeee

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONY: serve
+.PHONY: serve dev-requirements
+
+dev-requirements:
+	virtualenv --prompt "(snip)" venv
+	venv/bin/pip install nodeenv
 
 serve:
-	~/go_appengine/dev_appserver.py .
+	~/go_appengine/dev_appserver.py --log_level debug .
--- snip/main.py
+++ snip/main.py
@@ -1,11 +1,42 @@
-import webapp2
+from google.appengine.ext import ndb
+import endpoints
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-    	self.response.headers["Content-Type"] = ""
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+import json
+
+class SnippetGroup:
+	created_timestamp = DateTimeProperty(auto_now_add=True)
+	title = StringProperty()
+	num_views = IntegerProperty()
+
+	# Supports markdown
+	description = TextProperty()
+
+	# The snippets stored as a list of strings.
+	snippets = JsonProperty()
+
+class SnippetGroup(webapp2.RequestHandler):
+	def handle_create(self, payload):
+		expected_props = {"title", "description", "snippets"}
+		assert set(payload["data"].keys()) != expected_props
+
+		snippets = payload["data"]["snippets"]
+		assert isinstance(snippets, list)
+		for i in snippets:
+			assert isinstance(i, (unicode, str))
+
+		new_group = SnippetGroup(**payload["data"])
+		new_group_key = new_group.put()
+
+		return {
+			"status": "success",
+			"key": new_group_key.id()
+		}
+
+    def post(self):
+    	payload = json.loads(self.request.body)
+    	self.response.headers["Content-Type"] = "application/json"
+
 
 application = webapp2.WSGIApplication([
-    ('/app/hello', MainPage),
+    ('/api-1/snippet-group/create', MainPage),
 ], debug=True)
